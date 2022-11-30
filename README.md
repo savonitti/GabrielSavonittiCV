d70965e4b8d8d9f9ca751b5773fde88c54179bd6
savonitti <gabrielsavonitti@gmail.com>
2022-11-30 14:22:42 -0300

    First Commit - everything working

---
+++ README.md
@@ -0,0 +1,17 @@
+# CV Online
+
+>CV Gabriel Savonitti
+
+Projeto construido para treinar e poder me apresentar.
+Project built to train and present myself.
+
+![preview](./.github\preview.jpg)
+
+## 💼 Tecnologias
+
+- HTML
+- CSS
+- JS
+- Git e Github
+
+##
---
+++ scrollAnimation.js
@@ -0,0 +1,48 @@
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting){
+//             entry.target.classList.add('show');
+//         } 
+//     });
+// });
+
+//   var el = ["anchor0" , "anchor1", "anchor2", "anchor3", "anchor4"];
+//   var arrRect = [];
+//   for (let index = 0; index < el.length; index++) {
+//     var element = document.getElementById(el[index]);
+//     var rect = element.getBoundingClientRect();
+//     arrRect.push(rect);
+//   }
+
+  
+//   const list = document.querySelectorAll(".list");
+//   function activeLink() {
+//     list.forEach((item) => item.classList.remove("active"));
+//     this.classList.add("active");
+//   }
+//   list.forEach((item) => item.addEventListener("click", activeLink));
+
+
+// console.log(arrRect);
+// console.log(arrRect[2].top);
+  
+//   var bodyRect = document.body.getBoundingClientRect();
+//   var topHeader = 93;
+
+//     elemRect = element.getBoundingClientRect(),
+//     offset   = elemRect.top - bodyRect.top - topHeader;
+//     console.log(offset);
+
+
+//   window.addEventListener("scroll", (event) => {
+//         let scroll = this.scrollY;
+//         console.log(scroll)
+//     });
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
+
+
+      
+
